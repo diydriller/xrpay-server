9c55b51338a0761f67e7ceb834c8ebd618fc02a5
diydriller <dhrhd080@naver.com>
2025-09-25 11:26:45 +0900

    fix: module 구조 개선

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { WalletModule } from 'src/wallet/wallet.module';
 
-@Module({})
+@Module({
+  imports: [
+    PrismaModule,
+    WalletModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || '',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
 export class AuthModule {}
