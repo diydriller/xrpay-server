08ebbe5f15f9e82d4e8d145e6514246027f0628c
diydriller <dhrhd080@naver.com>
2025-10-04 03:00:31 +0900

    fix: transaction decorator를 AsyncLocalStorage로 재구현

--- src/common/decorator/transaction.decorator.ts
+++ src/common/decorator/transaction.decorator.ts
@@ -1,6 +1,23 @@
-import { applyDecorators, UseInterceptors } from '@nestjs/common';
-import { TransactionInterceptor } from '../interceptor/transaction.interceptor';
+import { asyncLocalStorage } from 'src/prisma/transaction-storage';
 
 export function Transactional() {
-  return applyDecorators(UseInterceptors(TransactionInterceptor));
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const original = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+      const existingTx = asyncLocalStorage.getStore();
+      if (existingTx) {
+        return await original.apply(this, args);
+      } else {
+        return this.prisma.$transaction(async (tx) => {
+          return asyncLocalStorage.run(tx, async () => {
+            return await original.apply(this, args);
+          });
+        });
+      }
+    };
+  };
 }
